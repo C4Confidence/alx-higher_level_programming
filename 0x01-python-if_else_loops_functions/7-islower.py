8077db0a6114a1c5f70f10bbb999324b22aff43c
C4Confidence <iamconfidence5@gmail.com>
2024-01-08 14:39:39 +0100

    islower

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    return ord('a') <= ord(c) <= ord('z')
-
-check_char = 'a'
-result = islower(check_char)
-print(f"{check_char}")
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return (False)
