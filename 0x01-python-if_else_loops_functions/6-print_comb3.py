0d6f224f2740424d215dec3529d1567a7a12616d
C4Confidence <iamconfidence5@gmail.com>
2024-01-08 14:33:52 +0100

    print combo

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for first_num in range(0, 10):
     for second_num in range(first_num + 1, 10):
         if first_num == 8 and second_num == 9:
-            print("{} {}".format(first_num, second_num))
+            print("{}{}".format(first_num, second_num))
         else:
-            print("{} {}, ".format(first_num, second_num), end='')
+            print("{}{}, ".format(first_num, second_num), end='')
