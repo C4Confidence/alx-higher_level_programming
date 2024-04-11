b41cb0bf9ff931894038b9cf763f9092a6c685ff
C4Confidence <iamconfidence@gmail.com>
2024-04-11 10:03:03 +0100

    Corrections

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,19 +1,11 @@
 #!/usr/bin/node
-
 const BaseSquare = require('./5-square');
 
 class Square extends BaseSquare {
-  constructor (size) {
-    super(size);
-  }
-
-  charPrint (c) {
-    if (c === undefined) {
-      c = 'X';
-    }
-
+  charPrint (c = 'X') {
+    const line = c.repeat(this.width);
     for (let i = 0; i < this.height; i++) {
-      console.log(c.repeat(this.width));
+      console.log(line);
     }
   }
 }
