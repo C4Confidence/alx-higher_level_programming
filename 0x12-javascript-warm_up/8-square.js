e18660eeeb13a253132a1472dcb03f5eb3000d89
C4Confidence <iamconfidence@gmail.com>
2024-04-12 13:18:54 +0100

    Square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const size = parseInt(process.argv[2]);
+
+if (isNaN(size)) {
+    console.log("Missing size");
+} else {
+    if (size > 0) {
+        for (let i = 0; i < size; i++) {
+            console.log('X'.repeat(size));
+        }
+    } else {
+        console.log("Missing size");
+    }
+}
