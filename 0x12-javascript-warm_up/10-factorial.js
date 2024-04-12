4122c7d04624d097786f6acfb3ba0c05687f0edc
C4Confidence <iamconfidence@gmail.com>
2024-04-12 13:48:05 +0100

    Factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 
-const factorial = n => isNaN(n) || n <= 1 ? 1 : n * factorial(n - 1);
+const factorial = function fact(n) {
+    return isNaN(n) || n <= 1 ? 1 : n * fact(n - 1);
+};
 
 const num = parseInt(process.argv[2]);
 
