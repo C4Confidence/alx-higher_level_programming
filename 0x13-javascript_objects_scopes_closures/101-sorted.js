65e7d9db6bcfa79b14260455b1b54d9ef355cea4
C4Confidence <iamconfidence@gmail.com>
2024-04-15 13:17:16 +0100

    Sorted OCcurences

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -5,11 +5,11 @@ const { dict } = require('./101-data');
 const occurrencesByUserId = {};
 
 for (const userId in dict) {
-    const occurrences = dict[userId];
-    if (!(occurrences in occurrencesByUserId)) {
-        occurrencesByUserId[occurrences] = [];
-    }
-    occurrencesByUserId[occurrences].push(userId);
+  const occurrences = dict[userId];
+  if (!(occurrences in occurrencesByUserId)) {
+    occurrencesByUserId[occurrences] = [];
+  }
+  occurrencesByUserId[occurrences].push(userId);
 }
 
 console.log(occurrencesByUserId);
