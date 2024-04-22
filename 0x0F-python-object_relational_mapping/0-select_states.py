f939641baad3c3d4bcb8418e3463dd4d6e3ba5b3
C4Confidence <iamconfidence@gmail.com>
2024-04-22 14:07:24 +0100

    Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,42 +1,33 @@
 #!/usr/bin/python3
-import MySQLdb
-import sys
+"""A module that reads the states from a db"""
 
-def list_states(username, password, database):
-    # Connect to MySQL server
-    db = MySQLdb.connect(
+import sys
+import MySQLdb
+
+
+def connection(username, password, db_name):
+    """The function that connects to the db"""
+
+    conn = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=username,
         passwd=password,
-        db=database
-    )
+        db=db_name,
+        charset="utf8")
 
-    # Create cursor object
-    cursor = db.cursor()
+    curr = conn.cursor()
 
-    # Execute SQL query to select all states
-    cursor.execute("SELECT * FROM states ORDER BY id")
+    curr.execute("SELECT * FROM states ORDER BY id ASC")
 
-    # Fetch all rows
-    states = cursor.fetchall()
+    query_rows = curr.fetchall()
 
-    # Display results
-    for state in states:
-        print(state)
+    for rows in query_rows:
+        print(rows)
 
-    # Close cursor and connection
-    cursor.close()
-    db.close()
+    curr.close()
+    conn.close()
 
-if __name__ == "__main__":
-    # Check if correct number of arguments is provided
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
 
-    # Extract command line arguments
-    username, password, database = sys.argv[1:]
-
-    # Call list_states function with provided arguments
-    list_states(username, password, database)
+if __name__ == '__main__':
+    connection(sys.argv[1], sys.argv[2], sys.argv[3])
