55d76fedcf4b444898ccb14cd6089e7c69d5356c
C4Confidence <iamconfidence5@gmail.com>
2024-05-30 13:26:04 +0100

    CisFun

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -6,9 +6,9 @@ const fs = require('node:fs');
 const file = process.argv[2];
 
 fs.readFile(file, 'utf-8', (err, data) => {
-	if (err) {
-		console.error(err);
-		return;
-	}
-	console.log(data);
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
 });
