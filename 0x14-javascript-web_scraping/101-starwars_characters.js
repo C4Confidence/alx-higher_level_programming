7828a0e61cd7b1e7e1d92d88282f8c890015c3ab
C4Confidence <iamconfidence5@gmail.com>
2024-05-30 16:21:43 +0100

    Right Order

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+const { argv } = require('process');
+const request = require('request');
+const util = require('util');
+
+const api = `https://swapi-api.alx-tools.com/api/films/${argv[2]}`;
+const requestPromise = util.promisify(request);
+
+request(api, { json: true }, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  }
+
+  async function fetchAllData (bc) {
+    for (const idx in bc) {
+      try {
+        const res = await requestPromise({ uri: bc[idx], json: true });
+        const resBody = res.body;
+        console.log(resBody.name);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  }
+
+  fetchAllData(body.characters);
+});
